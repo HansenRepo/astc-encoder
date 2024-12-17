58e28ff3ff2f7752d4aa9d0590b97db4cc5e0bf3
Hansen <heshanzhang@outlook.com>
2024-12-17 11:20:13 +0800

    [Hansen] init iOS project

---
+++ iOS/ASTC-iOS-Kit/ASTC_iOS_Kit.h
@@ -0,0 +1,18 @@
+//
+//  ASTC_iOS_Kit.h
+//  ASTC-iOS-Kit
+//
+//  Created by 张河山 on 2024/12/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ASTC_iOS_Kit.
+FOUNDATION_EXPORT double ASTC_iOS_KitVersionNumber;
+
+//! Project version string for ASTC_iOS_Kit.
+FOUNDATION_EXPORT const unsigned char ASTC_iOS_KitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ASTC_iOS_Kit/PublicHeader.h>
+
+
